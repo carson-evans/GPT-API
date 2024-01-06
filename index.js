ec4d128918cb5f6eb44638583feac4326189ffd1
carson-evans <carsonevans@outlook.com>
2024-01-06 12:25:08 -0500

    General Kenobi

--- index.js
+++ index.js
@@ -28,13 +28,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+// Configure OpenAI API key authorization: OPENAI_API_KEY from env.js
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
+// Create Express app
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('public'));
+
 
 app.get("/", (req, res) => {
   res.send("OpenAI Chat API is running. Use POST request to interact.");
@@ -42,17 +46,15 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
   try {
+
     const { message } = req.body;
-//    const userMessage = req.body.message;
+
     const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
-        {"role": "user", "content": `${message}`},
+        {role: "user", content: `${message}`},
       ]
     });
-    console.log(completion.choices[0]);
-
-    console.log("OpenAI API Response:", completion);
 
     res.json({
       completion: completion.choices[0].message,
